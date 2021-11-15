1980f9b5b7489936c675e876b7365648d8f3a580
SkeletalHunter <47721788+SkeletalHunter@users.noreply.github.com>
2021-11-15 07:18:32 +0300

    Add files via upload

--- js/Project_js.js
+++ js/Project_js.js
@@ -1,97 +1,3 @@
-function lab5_1(){
-    let arr = [];
-
-    for (let i = 0; i < 1000; i++) {
-        arr[i] = Math.floor(Math.random()*1000);
-    }
-    min(arr);
-    max(arr);
-    median(arr);
-    quickSortedArr = quickSort(arr, 0, arr.length - 1);
-    for (let i = 0; i < arr.length; i++) {
-        console.log(quickSortedArr[i]);
-    }
-    alert(`Задание завершено (просмотрите консоль)`);
-}
-function lab5_2(){
-    var tags = {};
-    var elements = document.getElementsByTagName('*');
-    var inputArr = Array.prototype.slice.call(elements);
-    for (let i = 0; i < inputArr.length; i++) {
-        if (tags[inputArr[i].tagName] == undefined) {
-            tags[inputArr[i].tagName] = 1;
-        }
-        else {
-            tags[inputArr[i].tagName] += 1;
-        }
-    }
-    for (key in tags) {
-        console.log(key + " - " + tags[key]);
-    }
-    alert(`Задание завершено (просмотрите консоль)`);
-}
-function min(arr) {
-    let current_min = Number.MAX_VALUE;
-    arr.forEach((element) => {
-        if (element < current_min) {
-            current_min = element
-        }
-    });
-    console.log(`Минимум: ${current_min}`);
-}
-
-function max(arr) {
-    let current_max = Number.MIN_VALUE;
-    arr.forEach((element) => {
-        if (element > current_max) {
-            current_max = element;
-        }
-    });
-    console.log(`Максимум: ${current_max}`);
-}
-
-function median(arr) {
-    arr.sort();
-    result = arr[Math.floor(arr.length / 2)];
-    console.log(`Медиана: ${result}`);
-}
-
-function quickSort(arr, left, right) {
-    let index;
-    if (arr.length > 1) {
-        index = partition(arr, left, right);
-        if (left < index - 1) {
-            quickSort(arr, left, index - 1);
-        }
-        if (index < right) {
-            quickSort(arr, index, right);
-        }
-    }
-    return arr;
-}
-
-function partition(arr, left, right) {
-    let pivot = arr[Math.floor((left + right) / 2)],
-        i = left,
-        j = right;
-    while (i <= j) {
-        while (arr[i] < pivot) {
-            i++;
-        }
-        while (arr[j] > pivot) {
-            j--;
-        }
-        if (i <= j) {
-            let tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-            i++;
-            j--;
-        }
-    }
-    return i;
-}
-
 function changeImage() {
     let images = ['1.JPG', '2.JPG'];
     let element = document.getElementById('avatar');
@@ -102,3 +8,21 @@ function changeImage() {
     let root = 'img/';
     element.src = root + images[index];
 }
+const findLoadTime = (function() {
+    document.getElementsByClassName("page-loaded-result")[0].innerText =
+        (window.performance.timing.domContentLoadedEventEnd -
+            window.performance.timing.navigationStart) / 1000;
+});
+window.addEventListener('load', (event) => {
+    findLoadTime();
+});
+window.addEventListener("load", function(event) {
+    var href = window.location.href;
+    var links = document.getElementsByClassName("footer-link");
+    for (var i = 0; i < links.length; i++) {
+        var linkHref = links[i].href;
+        if (linkHref == href) {
+            links[i].style.textDecoration = "underline";
+        }
+    }
+});
